34b51004852546b00d15c61c4fa91908890aabd4
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-03-29 22:02:22 -0300

    Criação de testes unitários

--- src/main/java/com/andregpereira/tests/springboottestcases/infra/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/andregpereira/tests/springboottestcases/infra/exceptions/UserAlreadyExistsException.java
@@ -1,11 +1,6 @@
 package com.andregpereira.tests.springboottestcases.infra.exceptions;
 
-import java.io.Serial;
-
 public class UserAlreadyExistsException extends RuntimeException {
-
-    @Serial
-    private static final long serialVersionUID = -3863700555653485145L;
 
     public UserAlreadyExistsException(String msg) {
         super(msg);
